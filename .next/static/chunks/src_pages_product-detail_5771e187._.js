22a925ddcaceb0c114d2c92be42202a9fe62e9f4
Yuqiries <88714627+Yuqiries@users.noreply.github.com>
2025-07-06 22:25:52 +0800

    Add .next/static/chunks/src_pages_product-detail_5771e187._.js 1751811951127

---
+++ .next/static/chunks/src_pages_product-detail_5771e187._.js
@@ -0,0 +1,10 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__303caab5._.js",
+  "static/chunks/node_modules_next_8e47a57f._.js",
+  "static/chunks/node_modules_react-dom_82bb97c6._.js",
+  "static/chunks/node_modules_4f9db5b5._.js"
+],
+    source: "entry"
+});
