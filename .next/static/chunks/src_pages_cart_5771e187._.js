2fbeef336d8e86c13b05c91f7108936b8dd8a111
Yuqiries <88714627+Yuqiries@users.noreply.github.com>
2025-07-06 22:25:36 +0800

    Add .next/static/chunks/src_pages_cart_5771e187._.js 1751811935528

---
+++ .next/static/chunks/src_pages_cart_5771e187._.js
@@ -0,0 +1,10 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_d07759ca._.js",
+  "static/chunks/node_modules_react-dom_82bb97c6._.js",
+  "static/chunks/node_modules_f2ab06f3._.js",
+  "static/chunks/[root-of-the-server]__09dbe551._.js"
+],
+    source: "entry"
+});
